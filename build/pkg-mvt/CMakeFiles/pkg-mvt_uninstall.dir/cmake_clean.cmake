9ecefb01c2d38141b3bad2487cd1cd76ae4e0bad
kykylechti <161956036+kykylechti@users.noreply.github.com>
2024-12-20 09:17:47 +0100

    Delete build directory

--- build/pkg-mvt/CMakeFiles/pkg-mvt_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/pkg-mvt_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/pkg-mvt_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
